0b21f0615777436e93a2fdc8ac1585d3a9dc9928
Shanshan Zhao <shanshan.zhao@Shanshans-MacBook-Pro.local>
2018-02-26 11:35:06 +0100

    [Feat] clean useless code

--- StudioStory/Cells/GroupPreviewCell.swift
+++ StudioStory/Cells/GroupPreviewCell.swift
@@ -17,42 +17,12 @@ class GroupPreviewCell: UICollectionViewCell {
     
     override func awakeFromNib() {
         configCellUI()
-        
-
     }
     
     private func configCellUI() {
-//        let layer = UIView(frame: self.frame)
-//        layer.layer.cornerRadius = 16
-//        layer.backgroundColor = UIColor.white
-//        let shadow0 = UIView(frame: CGRect(x: 0, y: 0, width: 325, height: 386))
-//        shadow0.layer.cornerRadius = 16
-//        shadow0.layer.shadowOffset = CGSize(width: 0, height: 8)
-//        shadow0.layer.shadowColor = UIColor(red:0, green:0, blue:0, alpha:0.15).cgColor
-//        shadow0.layer.shadowOpacity = 1
-//        shadow0.layer.shadowRadius = 21
-//        layer.addSubview(shadow0)
-//
-//        let shadow1 = UIView(frame: CGRect(x: 0, y: 0, width: 325, height: 386))
-//        shadow1.layer.cornerRadius = 16
-//        shadow1.layer.shadowOffset = CGSize(width: 0, height: 2)
-//        shadow1.layer.shadowColor = UIColor(red:0, green:0, blue:0, alpha:0.12).cgColor
-//        shadow1.layer.shadowOpacity = 1
-//        shadow1.layer.shadowRadius = 2
-//        layer.addSubview(shadow1)
-//
-//        let shadow2 = UIView(frame: CGRect(x: 0, y: 0, width: 325, height: 386))
-//        shadow2.layer.cornerRadius = 16
-//        shadow2.layer.shadowOffset = CGSize(width: 0, height: 4)
-//        shadow2.layer.shadowColor = UIColor(red:0, green:0, blue:0, alpha:0.15).cgColor
-//        shadow2.layer.shadowOpacity = 1
-//        shadow2.layer.shadowRadius = 7
-//        layer.addSubview(shadow2)
-//        self.addSubview(layer)
-//
         for subview in imagePreviewStackView.subviews {
             for item in subview.subviews {
-                if let image = item as? UIImageView{
+                if let image = item as? UIImageView {
                     image.layer.cornerRadius = 20
                 }
             }
